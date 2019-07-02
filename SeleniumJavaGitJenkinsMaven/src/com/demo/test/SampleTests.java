bfcf5593d723e8b6dddd51917dfb83ae5ff14c0d
athada <athada@ATHADAPF1HAGFU.opentext.net>
2019-07-02 15:42:44 +0530

    Added Listener

--- SeleniumJavaGitJenkinsMaven/src/com/demo/test/SampleTests.java
+++ SeleniumJavaGitJenkinsMaven/src/com/demo/test/SampleTests.java
@@ -1,17 +1,24 @@
 package com.demo.test;
 
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.demo.listeners.ExtendedITestListener;
+
+@Listeners(ExtendedITestListener.class)
 public class SampleTests {
 	
 	@Test
 	public void m1() {
 		System.out.println("M1");
+		Assert.assertTrue(true);
 	}
 	
 	@Test
 	public void m2() {
 		System.out.println("M2");
+		Assert.assertTrue(false);
 	}
 
 }
